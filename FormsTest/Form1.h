277b4dbd7e7fc1f75430f6afa49efef56c13f707
Kistelekig98 <kistelekig@t-online.hu>
2018-08-10 16:02:43 +0200

    input + label működtetése

--- FormsTest/Form1.h
+++ FormsTest/Form1.h
@@ -1,4 +1,5 @@
 #pragma once
+//#include <locale>
 
 namespace CppCLR_WinformsProjekt {
 
@@ -37,6 +38,9 @@ namespace CppCLR_WinformsProjekt {
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::Button^  button1;
 	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label2;
+
 	protected:
 
 	private:
@@ -55,35 +59,68 @@ namespace CppCLR_WinformsProjekt {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(11, 48);
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->textBox1->Location = System::Drawing::Point(66, 32);
 			this->textBox1->Margin = System::Windows::Forms::Padding(2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(68, 20);
+			this->textBox1->Size = System::Drawing::Size(177, 29);
 			this->textBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(11, 84);
+			this->button1->Font = (gcnew System::Drawing::Font(L"Tahoma", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(139, 79);
 			this->button1->Margin = System::Windows::Forms::Padding(2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(67, 26);
+			this->button1->Size = System::Drawing::Size(84, 34);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Button 1";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// label1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(11, 130);
-			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label1->BackColor = System::Drawing::Color::PaleTurquoise;
+			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(66, 134);
+			this->label1->MinimumSize = System::Drawing::Size(80, 20);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(177, 32);
 			this->label1->TabIndex = 2;
-			this->label1->Text = L"Felirat";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click_1);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->textBox2->Location = System::Drawing::Point(247, 32);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(41, 29);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label2
+			// 
+			this->label2->BackColor = System::Drawing::Color::PaleTurquoise;
+			this->label2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label2->Location = System::Drawing::Point(247, 134);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(41, 32);
+			this->label2->TabIndex = 4;
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// Form1
 			// 
@@ -91,16 +128,44 @@ namespace CppCLR_WinformsProjekt {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::Aqua;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(360, 207);
+			this->ClientSize = System::Drawing::Size(342, 189);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+
+
+
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		String ^ s;
+		int n;
+
+		if (textBox1->Text != "") {
+			s = textBox1->Text;
+			label1->Text = s;
+		}
+		if (textBox2->Text != "") {
+			n = System::Convert::ToInt32(textBox2->Text);
+			label2->Text = System::Convert::ToString(n);
+		}
+		
+		textBox1->Text = nullptr;
+		textBox2->Text = nullptr;
+	}
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void label1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+}
+};
 }
